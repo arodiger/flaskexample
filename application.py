6612d08b1124d6f5794b58acd3dfc72a17bbc8c0
Anthony Rodiger <anthony_rodiger@yahoo.com>
2022-12-11 18:08:05 -0600

    output string updated

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ application = Flask(__name__)
 @application.route('/')
 
 def index():
-    return " *****   APPLICATION.PY    Testing Flask Example on AWS Network ******* "
+    return " ***** application.py,  Flask Example uploaded to github, Code-pipeline created with auto-update to elastic beanstalk environment redirected to anthonyrodiger.com dns on AWS Network     ******* "
 
 if __name__ == "__main__":
     application.run(debug=False)
