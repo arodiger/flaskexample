e066353ff8bcb48d6719661b923725651c88ca5f
Anthony Rodiger <anthony_rodiger@yahoo.com>
2022-12-14 16:25:39 -0600

    added views.py

--- application.py
+++ application.py
@@ -1,11 +1,16 @@
 from flask import Flask
+from views import views
+
 
 application = Flask(__name__)
 
-@application.route('/')
+#setup my views.py file to handle the direction of pages
+application.register_blueprint(views,url_prefix="/")
 
-def index():
-    return " ***** application.py,  Flask Example uploaded to github, Code-pipeline created with auto-update to elastic beanstalk environment redirected to anthonyrodiger.com dns on AWS Network     ******* "
+
+# @application.route('/')
+# def index():
+#     return " ***** application.py,  Flask Example uploaded to github, Code-pipeline created with auto-update to elastic beanstalk environment redirected to anthonyrodiger.com dns on AWS Network     ******* "
 
 if __name__ == "__main__":
     application.run(debug=False)
---
+++ views.py
@@ -0,0 +1,9 @@
+from flask import Blueprint
+
+views = Blueprint(__name__, "views")
+
+@views.route('/')
+def home():
+    return "Inside views.py"
+
+
