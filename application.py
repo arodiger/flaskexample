360217f259ed14fa655949b30266c48a49f83a71
Anthony Rodiger <anthony_rodiger@yahoo.com>
2022-12-21 11:18:52 -0600

    added users resource and delete all

--- application.py
+++ application.py
@@ -7,13 +7,12 @@ application = Flask(__name__)
 application.register_blueprint(views,url_prefix="/")    #setup my views.py file to handle the direction of pages
 api = Api(application)                                  #wrap our application with the api
 
-video_put_args = reqparse.RequestParser()
+video_put_args = reqparse.RequestParser()               #utilize RequestParser class which will parse out data
 
 video_put_args.add_argument('name', type=str, help='Name of video is required', required=True)
 video_put_args.add_argument("views", type=int, help="Views of video is required", required=True)
 video_put_args.add_argument("likes", type=int, help="Likes on video is required", required=True)
 
-#videos = {1:{"name":"arod name", "views":"arod views", "likes":"arod likes"}}
 videos = {}
 
 def abortIfVideoIdDoesntExist(video_id):
@@ -23,6 +22,11 @@ def abortIfVideoIdDoesntExist(video_id):
 def abortIfVideoExists(video_id):
     if video_id in videos: 
         abort(409, message="Video already exists with that id")
+
+class Users(Resource):
+    def get(self, user_id):
+        print("inside get of tempUser")
+        return "", 200
 
 
 class Video(Resource):                                  #create resource class and methods that will satisfy api calls
@@ -38,13 +42,15 @@ class Video(Resource):                                  #create resource class a
         videos[video_id] = args
         return videos[video_id], 201
 
-    def delete(self, video_id):
+    def delete(self, video_id):                         #modified to delete all
         abortIfVideoIdDoesntExist(video_id)
-        del videos[video_id]
+        videos.clear()                                  #modified to delete all
+        # del videos[video_id]
         return "", 204
 
 
-api.add_resource(Video, "/video/<int:video_id>")        #register this class as a resource
+api.add_resource(Video, "/video/<int:video_id>")        #register video class as a resource
+api.add_resource(Users, "/Users/<int:user_id>")         #register user class as a resource
 
 if __name__ == "__main__":
     application.run(debug=True)
